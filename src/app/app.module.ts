6aae3146fef25d738055d7c8894cabe67f7fe503
lorenzokyne <l.capocchiano@studenti.uniba.it>
2020-09-19 10:57:11 +0200

    Added some material things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+//material Angular
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+export class PizzaPartyAppModule { }
+
+const materialModules = [
+  MatListModule,
+  MatButtonModule,
+  MatIconModule
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +25,9 @@ import { SideNavComponent } from './side-nav/side-nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
